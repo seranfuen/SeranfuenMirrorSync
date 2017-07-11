0dc0ffbbec23f8fd5b1e029ba48564d2a412dcf4
Sergio Ángel Verbo <seranfuen@gmail.com>
2017-07-11 21:14:40 +0200

    Started project

---
+++ SeranfuenMirrorSyncLib/Controllers/ScheduleController.cs
@@ -0,0 +1,33 @@
+﻿using SeranfuenMirrorSyncLib.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeranfuenMirrorSyncLib.Controllers
+{
+    public class ScheduleController
+    {
+        #region ' Singleton '
+
+        private static ScheduleController _instance;
+        private static object oLock = new object();
+
+        public static ScheduleController Instance
+        {
+            get
+            {
+                lock (oLock)
+                {
+                    if (_instance == null) _instance = new ScheduleController();
+                    return _instance;
+                }
+            }
+        }
+
+        #endregion
+
+        private HashSet<FolderSyncSetting> settings;
+    }
+}
---
+++ SeranfuenMirrorSyncLib/Data/FolderSyncSetting.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeranfuenMirrorSyncLib.Data
+{
+    [DataContract]
+    [Serializable]
+    public class FolderSyncSetting
+    {
+        [DataMember]
+        public string SourceRootFolder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string MirrorRootFolder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? SyncTime1
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? SyncTime2
+        {
+            get;
+            set;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            if (this == obj) return true;
+            var typedObject = (FolderSyncSetting)obj;
+            return typedObject.SourceRootFolder.Equals(SourceRootFolder, StringComparison.OrdinalIgnoreCase) && 
+                typedObject.MirrorRootFolder.Equals(MirrorRootFolder, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return 13 * SourceRootFolder.GetHashCode() + 17 * MirrorRootFolder.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Source: '{0}' - Mirror: '{1}'", SourceRootFolder, MirrorRootFolder);
+        }
+    }
+}
