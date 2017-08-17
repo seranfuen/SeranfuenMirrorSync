b73f97f6cb086a1957649984dcc584377cc16da6
Sergio Ángel Verbo <seranfuen@gmail.com>
2017-08-17 21:18:22 +0200

    Ran some tests adding a progress bar to a DataGrid. Easy to do, it will go to the status window

---
+++ SeranfuenMirrorSync/Test/ViewModels/ProgressTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeranfuenMirrorSync.Test.ViewModels
+{
+    class ProgressTest : INotifyPropertyChanged
+    {
+        private Timer _timer;
+        private int _progress = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ProgressTest()
+        {
+
+        }
+
+        public void StartTest()
+        {
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+            }
+            _progress = 0;
+            _timer = new Timer();
+            _timer.Interval = 100;
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        public int Progress
+        {
+            get { return _progress; }
+            set
+            {
+                _progress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Progress"));
+            }
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            Progress++;
+            if (Progress >= 100)
+            {
+                _timer.Stop();
+            }
+        }
+    }
+}
---
+++ SeranfuenMirrorSync/Test/WndProgressBarInGridTest.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SeranfuenMirrorSync.Test.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeranfuenMirrorSync.Test
+{
+    /// <summary>
+    /// Interaction logic for WndProgressBarInGridTest.xaml
+    /// </summary>
+    public partial class WndProgressBarInGridTest : Window
+    {
+        private ProgressTest _test;
+
+        public WndProgressBarInGridTest()
+        {
+            InitializeComponent();
+            _test = new ProgressTest();
+            DataContext = new List<ProgressTest>()
+            {
+                _test
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _test.StartTest();
+        }
+    }
+}
